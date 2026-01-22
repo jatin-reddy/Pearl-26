a9bc7c4a7171151df98aa7eb92531f8a30a42035
Jatin Reddy <srijatin@gmail.com>
2026-01-22 23:52:14 +0530

    setup: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <></>;
-}
+import { Outlet } from "react-router-dom";
+// import Navbar from "./components/Nav";
+// import Footer from "./components/Foooter";
+
+const App = () => {
+  return (
+    <>
+      {/*<Navbar /> */}
+      <main>
+        <Outlet />
+      </main>
+      {/* <Footer /> */}
+    </>
+  );
+};
 
 export default App;
---
+++ src/router.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+
+import Landing from "./pages/Landing";
+import Events from "./pages/Events";
+import Schedule from "./pages/Schedule";
+import Gallery from "./pages/Gallery";
+import Team from "./pages/Team";
+import Contact from "./pages/Contact";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Landing /> },
+      { path: "events", element: <Events /> },
+      { path: "schedule", element: <Schedule /> },
+      { path: "gallery", element: <Gallery /> },
+      { path: "team", element: <Team /> },
+      { path: "contact", element: <Contact /> },
+    ],
+  },
+]);

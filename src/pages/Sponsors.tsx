a27871f56e51a655a309f9b4ad4e69f746f9ac30
Jatin Reddy <srijatin@gmail.com>
2026-02-02 17:37:44 +0530

    feat: coming soon

--- src/pages/Sponsors.tsx
+++ src/pages/Sponsors.tsx
@@ -1,5 +1,18 @@
+import SectionHeading from "../components/SectionHeading";
+
 const Sponsors = () => {
-  return <div>Sponsors</div>;
+  return (
+    <div className="h-screen bg-[#634AA7] flex flex-col items-center justify-center">
+      <SectionHeading
+        text1="Our"
+        text2="Events"
+        ghostText="Our Events"
+        text1Color="text-[#FFFFFF]"
+        text2Color="text-[#F3D300]"
+      />
+      <p className="text-white/70 mt-4">We're lining up something great!</p>
+    </div>
+  );
 };
 
 export default Sponsors;

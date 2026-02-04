6488e5c813f8efee8ee4a600c8856f1866a83413
Jatin Reddy <srijatin@gmail.com>
2026-02-05 05:20:41 +0530

    fix: gallery merge errors

--- src/pages/Sponsors.tsx
+++ src/pages/Sponsors.tsx
@@ -1,18 +1,5 @@
+import { motion } from "motion/react";
 import SectionHeading from "../components/SectionHeading";
-
-const Sponsors = () => {
-  return (
-    <div className="h-screen bg-[#634AA7] flex flex-col justify-center">
-      <SectionHeading
-        text1="Coming"
-        text2="Soon"
-        ghostText="Coming Soon"
-        text1Color="text-[#FFFFFF]"
-        text2Color="text-[#F3D300]"
-      />
-    </div>
-  );
-};
 
 export default function Sponsors() {
   return (
